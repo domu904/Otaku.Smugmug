c9c6c9284c7c1bd3f0991a211eea0ba2e9c6f42b
otaku904 <domu904@gmail.com>
2019-06-20 19:44:45 -0500

    Initial commit

---
+++ Otaku.Smugmug.Test/PictureTest.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class PictureTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ScanGalleries()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void DownloadPictures()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void UpdateTags()
+        {
+            Assert.Pass();
+        }
+    }
+}
