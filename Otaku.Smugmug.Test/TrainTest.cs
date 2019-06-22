fbc6599d25b36dceccd67650b4d325793edf4df8
otaku904 <domu904@gmail.com>
2019-06-22 18:44:47 -0500

    Create TrainTest.cs

---
+++ Otaku.Smugmug.Test/TrainTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Otaku.Smugmug.Test
+{
+    public class TrainTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TrainModelTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void UpdateLearnedModelTest()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
